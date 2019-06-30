8662029886dad41405c63d1bc70bd4dc034effcf
Mohit Kumar <mhtkmr303@gmail.com>
2019-06-30 12:48:27 +0530

    payment resolution

--- models/razorpayQuery.js
+++ models/razorpayQuery.js
@@ -10,9 +10,16 @@ class response{
   }
   static async paymentres(resp){
     // const payment = new Payment(resp);
-    const payment = Payment.findOneAndUpdate({accountId: resp.accountId}, resp)
-    const res = await payment.save();
-    return res;
+    const payment = await Payment.findOne({accountId: resp.accountId});
+    if(!payment){      
+      const pay = new Payment(resp);      
+      const res = await pay.save();
+      return res;
+    } else {
+      const res = payment.payload.push(resp.payload);      
+      const pay =await Payment.findOneAndUpdate({accountId: payment.accountId}, payment);            
+      return pay;
+    }
   }
   static async tokenres(resp){
     const token = new Token(resp);
